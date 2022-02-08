0733b7b36cfa4d50a44bb85862012e401b9761ca
abigailGadams <abigailgadams0@gmail.com>
2022-02-08 00:41:23 -0800

    updating to check for whitespace in passwords

--- password.py
+++ password.py
@@ -3,44 +3,6 @@ import string
 import re # regular expression
 
 
-# while True:
-#   #Taking in password from user
-#   user_input = input("Enter a password : ")
-#   is_valid = False
-
-#   if (len(user_input)<6 or len(user_input)>12):
-#     #Check number of characters 
-#     print("Not valid ! Total characters should be between 6 and 12")
-#     continue
-#   elif not re.search("[A-Z]",user_input):
-#       #Check uppercase letter password requirement
-#     print("Not valid ! It should contain one letter between [A-Z]")
-#     continue
-#   elif not re.search("[a-z]",user_input):
-#     #Check lowercase letter password requirement
-#     print("Not valid ! It should contain one letter between [a-z]")
-#     continue
-#   elif not re.search("[1-9]",user_input):
-#     #7
-#     print("Not valid ! It should contain one letter between [1-9]")
-#     continue
-#   elif not re.search("[~!@#$%^&*]",user_input):
-#     #8
-#     print("Not valid ! It should contain at least one letter in [~!@#$%^&*]")
-#     continue
-#   elif re.search("[\s]",user_input):
-#     #9
-#     print("Not valid ! It should not contain any space")
-#     continue
-#   else:
-#     #10
-#     is_valid = True
-#     break
-
-# if(is_valid):
-#       print("Password is valid")
-
-
 def password_check(password):
 
     Symbols =['$', '@', '#', '%', "!"]
@@ -66,6 +28,9 @@ def password_check(password):
     elif not any(char in Symbols for char in password):
         print('Password should have at least one of the symbols $@!#')
         valid_password = False  
+    elif re.search("[\s]",password):
+        print("Password should not contain any spaces")
+        valid_password = False
            
     else:
         print('This password is valid!')
@@ -78,7 +43,7 @@ def password_check(password):
 # Main method
 def main():
     password = input('Enter your new password: ')
-    # passwd = 'Geek12@'
+    
       
     if (password_check(password)):
         print("Password is valid")
@@ -89,39 +54,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-# import string
-# import re # regular expression
-
-
-# for letter in string.ascii_lowercase:
-#     print(letter, end =" ")
-# for letter in string.ascii_uppercase:
-#     print(letter, end =" ")
-
-
-
-# password = input('Enter your new password: ')
-# characters = ['$','@','#']
-# numbers = [range(0,9)]
-# lowercase = string.ascii_lowercase 
-# uppercase = string.ascii_uppercase
-
-
-# def password_check(characters, numbers, lowercase,uppercase):
-#     valid_password =  True # evaluates to 0
-    
-#     for i in password:
-#         if len(password) < 6 | len(password>16):
-#             i += 1
-#         elif numbers not in password:
-#             valid_password = False
-#         elif lowercase not in password:
-#             valid_password = False
-#         elif uppercase not in password:
-#             valid_password = False
-#         else:
-#             print('This password is valid!')
-#             print(valid_password)
-            
-# password_check(password)
-            
